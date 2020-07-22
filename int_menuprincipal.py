e3c7cd8476f0080888a8edc44ab15ffeb5430153
Israel Zambrano <izambranod@unemi.edu.ec>
2020-07-22 13:41:49 -0500

    first commit

---
+++ int_menuprincipal.py
@@ -0,0 +1,41 @@
+from funciones import menu
+from colorama import Fore, init
+init(autoreset=True)
+import os 
+def ejecutar_menu():
+    Integrantes()
+    opc = ' '
+    while True:
+        opc =  str(menu(
+            ['Grupo','Plan de cuenta','Salir'],
+            Fore.GREEN + '      MENU PRINCIPAL   '))
+        if opc == '0':
+            print(Fore.GREEN +'\n<<<Ha Ingresado al menu grupo de cuenta>>> ')
+            from int_grupo import ejecutar_grupo
+            ejecutar_grupo()
+            input('Presione una tecla para continuar')
+        elif opc == '1':
+            print(Fore.GREEN + '\n<<<Ha Ingresado al menu Plan de cuenta>>> ')
+            from int_cuenta import ejecutar_cuenta
+            ejecutar_cuenta()
+            input('Presione una tecla para continuar')
+        elif opc == '2':
+            answer=input(f"¿Estas seguro que desea salir del sistema? [y/n]: ")
+            if not answer or answer[0].lower() != 'y':
+                input('Presione una tecla para continuar')
+            else:
+                print(Fore.GREEN + '\n<<<Gracias por usar el Sistema>>> ')
+                exit( )
+                break
+
+def Integrantes():
+    print(Fore.GREEN + '*'*30)
+    print(Fore.GREEN + '   INTEGRANTES DEL DEBER:      ')
+    print(Fore.GREEN + '*'*30)
+    print(Fore.BLUE + '  José Arreaga Salvatierra ')
+    print(Fore.BLUE + '  William Garcia Sojos ')
+    print(Fore.BLUE + '  Israel Zambrano Dominguez ')
+    
+
+       
+ejecutar_menu()
